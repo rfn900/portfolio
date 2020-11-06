7722abfd0bf5f16d1b29a9845801ae9bd712c914
Rodrigo Nascimento <rfn900@gmail.com>
2020-11-06 10:37:33 +0100

    Fixed browser compatibility bugs and add extra design elements

--- js/script.js
+++ js/script.js
@@ -22,10 +22,13 @@ document.addEventListener("DOMContentLoaded", ()=>{
     ) {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          console.log(entry.target.id);
+          //console.log(entry.target.id);
           document.getElementById(`${entry.target.id}-link`).style.color = "#F6B21B";
+          //document.getElementById(`${entry.target.id}-link`).classList.add("active-menu-link");
         } else {
-            document.getElementById(`${entry.target.id}-link`).style.color = "";
+          document.getElementById(`${entry.target.id}-link`).style.color = "";
+
+         // document.getElementById(`${entry.target.id}-link`).classList.remove("active-menu-link");
         }
       });
     },
@@ -53,5 +56,20 @@ document.addEventListener("DOMContentLoaded", ()=>{
     
     sectionOneObserver.observe(sectionOne);
     
+
+    let img_card = document.getElementById("card-img");
+    let img = document.querySelector("#card-img img");
+    img.addEventListener("mousemove", e=> {
+      let xAxis = (e.pageX-img_card.offsetLeft-img_card.offsetWidth/2)/5;
+      let yAxis = (e.pageY-img_card.offsetTop-img_card.offsetHeight/2)/5 ;
+      img_card.style.transform = `rotateY(${xAxis}deg) rotateX(${-1*yAxis}deg)`;
+      console.log(-1*xAxis,yAxis)
+    })
+
+
+
+
+
+
 })
 
