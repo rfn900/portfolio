95397b010f249d3993a1139ddd23e8b092f66573
rfn900 <rfn900@gmail.com>
2021-09-21 13:35:35 +0200

    Fix smooth scrolling on mobile

--- js/script.js
+++ js/script.js
@@ -1,131 +1,144 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
+  const nav = document.querySelector("nav");
+  const sectionOne = document.querySelector(".hero");
+  const sections = Array.from(document.getElementsByTagName("section"));
+  const navLinks = Array.from(document.getElementsByClassName("nav-links"));
+  const nav_toggle_checkbox = document.getElementById("toggle-checkbox");
+  const scroll_button_link = document.getElementById("scroll-button-link");
 
-    const nav = document.querySelector("nav");
-    const sectionOne = document.querySelector(".hero");
-    const sections = Array.from(document.getElementsByTagName("section"));
-    const nav_toggle_checkbox = document.getElementById("toggle-checkbox");
+  //I use the intersection observer to control for the different sections of my Single Page Application
+  const sectionOneOptions = {
+    rootMargin: "-100% 0px 0px 0px",
+  };
+  const sectionOptions = {
+    rootMargin: "0px",
+    threshold: 0.2,
+  };
 
-    //I use the intersection observer to control for the different sections of my Single Page Application
-    const sectionOneOptions = {
-     
-      rootMargin: "-100% 0px 0px 0px",
-     
-    };
-    const sectionOptions = {
-     
-      rootMargin: "0px",
-      threshold: 0.2
-     
-    };
-    
-    const sectionObserver = new IntersectionObserver(function(
-      entries,
-      sectionObserver
-    ) {
-      entries.forEach(entry => {
-        if (entry.intersectionRatio > 0.2) document.getElementById(`${entry.target.id}-link`).style.color = "";
+  const sectionObserver = new IntersectionObserver(function (
+    entries,
+    sectionObserver
+  ) {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0.2)
+        document.getElementById(`${entry.target.id}-link`).style.color = "";
 
-        if (entry.isIntersecting) {
-          //Animate in elements when entering the about-section
-          if(entry.target.id == "about-section"){
-            document.querySelector(".img-wrapper").style.transform = "translateX(0%)"
-            document.querySelector(".main-content .text").style.transform = "translateX(0%)"
-            document.querySelector(".card-icons").style.transform = "translateZ(60px) translate(10px,130px) rotateZ(-2deg)"
-            
-          }
-          //Animate in elements when entering the skills-section
-          if(entry.target.id == "skills-section"){
-              document.querySelectorAll(".skill-filter-item").forEach(item=>{
-              item.style.transform = "translateX(0%)"
-            })
-          }
-          //Animate in elements when entering the portfolio-section
-          if(entry.target.id == "portfolio-section"){
-            document.querySelectorAll(".portfolio-item").forEach(item => {
-                item.style.transform = "translateX(0%)"
-              });
-          }
-          document.getElementById(`${entry.target.id}-link`).style.color = "";
-          document.getElementById(`${entry.target.id}-link`).style.color = "#F6B21B";
-          console.log(entry)
-        } else {
-          document.getElementById(`${entry.target.id}-link`).style.color = "";
+      if (entry.isIntersecting) {
+        //Animate in elements when entering the about-section
+        if (entry.target.id == "about-section") {
+          document.querySelector(".img-wrapper").style.transform =
+            "translateX(0%)";
+          document.querySelector(".main-content .text").style.transform =
+            "translateX(0%)";
+          document.querySelector(".card-icons").style.transform =
+            "translateZ(60px) translate(10px,130px) rotateZ(-2deg)";
         }
-      });
-    },
-    sectionOptions);
-
-    sections.forEach(section =>{
-        sectionObserver.observe(section);
-    })
-
-    const sectionOneObserver = new IntersectionObserver(function(
-      entries,
-      sectionOneObserver
-    ) {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          
-            nav.classList.remove("scrolled");  
-            document.querySelector(".menulist span").style.backgroundColor = "#ffffff"
-            document.querySelector(".scroll-up-button").classList.remove("scroll-up-pop");
-        } else {
-          if(!nav_toggle_checkbox.checked){
-            document.querySelector(".scroll-up-button").classList.add("scroll-up-pop");
-            nav.classList.add("scrolled"); 
-            document.querySelector(".menulist span").style.backgroundColor = "#ffffff"
-          }
-          
-          
+        //Animate in elements when entering the skills-section
+        if (entry.target.id == "skills-section") {
+          document.querySelectorAll(".skill-filter-item").forEach((item) => {
+            item.style.transform = "translateX(0%)";
+          });
         }
-      });
-    },
-    sectionOneOptions);
-    
-    sectionOneObserver.observe(sectionOne);
-    
+        //Animate in elements when entering the portfolio-section
+        if (entry.target.id == "portfolio-section") {
+          document.querySelectorAll(".portfolio-item").forEach((item) => {
+            item.style.transform = "translateX(0%)";
+          });
+        }
+        document.getElementById(`${entry.target.id}-link`).style.color = "";
+        document.getElementById(`${entry.target.id}-link`).style.color =
+          "#F6B21B";
+        console.log(entry);
+      } else {
+        document.getElementById(`${entry.target.id}-link`).style.color = "";
+      }
+    });
+  },
+  sectionOptions);
 
-    let collapse_bar = document.getElementById("collapse-bar");
-    let lateral_bar_icons = document.getElementById("lateral-bar-icons");
-    let lateral_bar = document.getElementById("lateral-bar");
-    let arrow = document.querySelector("#collapse-bar i");
-    collapse_bar.addEventListener("click", e => {
+  sections.forEach((section) => {
+    sectionObserver.observe(section);
+  });
 
-      lateral_bar_icons.classList.toggle("collapse");
-      arrow.classList.toggle("rotate-arrow-toggle");
-      
-    })
+  const sectionOneObserver = new IntersectionObserver(function (
+    entries,
+    sectionOneObserver
+  ) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        nav.classList.remove("scrolled");
+        document.querySelector(".menulist span").style.backgroundColor =
+          "#ffffff";
+        document
+          .querySelector(".scroll-up-button")
+          .classList.remove("scroll-up-pop");
+      } else {
+        if (!nav_toggle_checkbox.checked) {
+          document
+            .querySelector(".scroll-up-button")
+            .classList.add("scroll-up-pop");
+          nav.classList.add("scrolled");
+          document.querySelector(".menulist span").style.backgroundColor =
+            "#ffffff";
+        }
+      }
+    });
+  },
+  sectionOneOptions);
 
-    //Event listener for the mobile menu
-    nav_toggle_checkbox.addEventListener("change", ()=>{
-      if(nav_toggle_checkbox.checked)collapse_bar.style.display = "none";
-      else collapse_bar.style.display ="block"
-        document.querySelector(".navbar-menu").classList.toggle("navbar-menu-collapse");
-    })
+  sectionOneObserver.observe(sectionOne);
 
-    document.querySelectorAll(".navbar-menu li a").forEach(item => {
-      item.addEventListener("click", e=>{
-        nav.classList.add("scrolled");
-        nav_toggle_checkbox.checked=false;
-        collapse_bar.style.display ="block"
-        document.querySelector(".navbar-menu").classList.remove("navbar-menu-collapse");
-      })  
+  let collapse_bar = document.getElementById("collapse-bar");
+  let lateral_bar_icons = document.getElementById("lateral-bar-icons");
+  let lateral_bar = document.getElementById("lateral-bar");
+  let arrow = document.querySelector("#collapse-bar i");
+  collapse_bar.addEventListener("click", (e) => {
+    lateral_bar_icons.classList.toggle("collapse");
+    arrow.classList.toggle("rotate-arrow-toggle");
+  });
 
-    })
+  //Event listener for the mobile menu
+  nav_toggle_checkbox.addEventListener("change", () => {
+    if (nav_toggle_checkbox.checked) collapse_bar.style.display = "none";
+    else collapse_bar.style.display = "block";
+    document
+      .querySelector(".navbar-menu")
+      .classList.toggle("navbar-menu-collapse");
+  });
 
-    const projects_button = document.getElementById("go-to-projects");
-    projects_button.addEventListener("click",e=>{
-      window.location.href="#portfolio-section"
-    })
+  document.querySelectorAll(".navbar-menu li a").forEach((item) => {
+    item.addEventListener("click", (e) => {
+      nav.classList.add("scrolled");
+      nav_toggle_checkbox.checked = false;
+      collapse_bar.style.display = "block";
+      document
+        .querySelector(".navbar-menu")
+        .classList.remove("navbar-menu-collapse");
+    });
+  });
 
-    const modal_link = document.getElementById("link-for-modal");
-    const close_span = document.querySelector(".modal-content span")
-    modal_link.addEventListener("click", () => {
-      document.querySelector(".program-description-modal").classList.add("show")
-     // document.querySelector(".program-description-modal").style.opacity = "1"; 
-    })
-    close_span.addEventListener("click", ()=>{
-      document.querySelector(".program-description-modal").classList.remove("show")
-    })
-})
+  const projects_button = document.getElementById("go-to-projects");
+  projects_button.addEventListener("click", () => {
+    sections[3].scrollIntoView({ behavior: "smooth" });
+  });
 
+  const modal_link = document.getElementById("link-for-modal");
+  const close_span = document.querySelector(".modal-content span");
+  modal_link.addEventListener("click", () => {
+    document.querySelector(".program-description-modal").classList.add("show");
+    // document.querySelector(".program-description-modal").style.opacity = "1";
+  });
+  close_span.addEventListener("click", () => {
+    document
+      .querySelector(".program-description-modal")
+      .classList.remove("show");
+  });
+  navLinks.forEach((link, index) => {
+    link.addEventListener("click", () => {
+      sections[index].scrollIntoView({ behavior: "smooth" });
+    });
+  });
+  scroll_button_link.addEventListener("click", () => {
+    sections[0].scrollIntoView({ behavior: "smooth" });
+  });
+});
